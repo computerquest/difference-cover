b08db9f7d1b548b0b6c98a4f087b3e2646bf7a23
J Stigter <jaredstigter@gmail.com>
2018-11-04 16:20:24 -0500

    working on the new mpi

--- ConsoleApplication1/ConsoleApplication1/main.cpp
+++ ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -28,194 +28,248 @@ int id; //the id of this process
 int ierr;
 int p; //number of connected nodes
 
+const int found_tag = 42;
+MPI_Request found_request;
+int* found_item = new int[p];
+
 /*COMMANDS TO RUN/COMPILE
  * COMPILE: mpicxx \-o main main.cpp
  * RUN: mpiexec \-n [NUMBER OF INSTANCES] main [PARAMETERS]
  * EXAMPLE: mpiexec \-n 4 main stuff.txt 100
  */
 int main(int argc, char *argv[]) {
-    ierr = MPI_Init(&argc, &argv);
-    ierr = MPI_Comm_size(MPI_COMM_WORLD, &p);
-    ierr = MPI_Comm_rank(MPI_COMM_WORLD, &id);
-    if (argc == 1) {
-        cout << "This program computes difference covers of a range specified by" << endl;
-        cout << "the user. This program then saves the covers to a file that you" << endl;
-        cout << "specify. The following format is used to execute this command." << endl;
-    }
-    if (argc < 3) {
-        cout << "Usage: mhonors <filename> <startValue> [numberToCompute]" << endl << endl;
-        cout << "<filename> - enter a filename." << endl << endl;
-        cout << "<startValue> - enter an integer number. This is the first cover" << endl;
-        cout << "               the program will attempt to find." << endl << endl;
-        cout << "[numberToCompute] - enter an integer number. This field is optional, and" << endl;
-        cout << "                    will tell the program to compute this many covers," << endl;
-        cout << "                    starting at <startValue>. If you choose not to provide" << endl;
-        cout << "                    this value, the default value is 10." << endl;
-        return 0;
-    }
+	ierr = MPI_Init(&argc, &argv);
+	ierr = MPI_Comm_size(MPI_COMM_WORLD, &p);
+	ierr = MPI_Comm_rank(MPI_COMM_WORLD, &id);
+	if (argc == 1) {
+		cout << "This program computes difference covers of a range specified by" << endl;
+		cout << "the user. This program then saves the covers to a file that you" << endl;
+		cout << "specify. The following format is used to execute this command." << endl;
+	}
+	if (argc < 3) {
+		cout << "Usage: mhonors <filename> <startValue> [numberToCompute]" << endl << endl;
+		cout << "<filename> - enter a filename." << endl << endl;
+		cout << "<startValue> - enter an integer number. This is the first cover" << endl;
+		cout << "               the program will attempt to find." << endl << endl;
+		cout << "[numberToCompute] - enter an integer number. This field is optional, and" << endl;
+		cout << "                    will tell the program to compute this many covers," << endl;
+		cout << "                    starting at <startValue>. If you choose not to provide" << endl;
+		cout << "                    this value, the default value is 10." << endl;
+		return 0;
+	}
 
-    int startValue = atoi(argv[2]);
-    int numberToCompute = 10;
+	int startValue = atoi(argv[2]);
+	int numberToCompute = 10;
 
-    if (argc == 4)
-        numberToCompute = atoi(argv[3]);
-    ofstream out;
-    out.open(argv[1], ios::out);
-    if (!out) {
-        cerr << "File could not be opened." << endl;
-        exit(1);
-    } // end if
-    cout << setw(4) << "p" << setw(9) << "f(p)" << setw(37) << "difference cover" << endl;
-    out << setw(4) << "p" << setw(9) << "f(p)" << setw(37) << "difference cover" << endl;
+	if (argc == 4)
+		numberToCompute = atoi(argv[3]);
+	ofstream out;
+	out.open(argv[1], ios::out);
+	if (!out) {
+		cerr << "File could not be opened." << endl;
+		exit(1);
+	} // end if
+	cout << setw(4) << "p" << setw(9) << "f(p)" << setw(37) << "difference cover" << endl;
+	out << setw(4) << "p" << setw(9) << "f(p)" << setw(37) << "difference cover" << endl;
 
-    int instanceStart = numberToCompute/p;
-    if(id < numberToCompute%p) {
-        instanceStart += 1;
+	int instanceStart = numberToCompute / p;
+	if (id < numberToCompute%p) {
+		instanceStart += 1;
 
-        if(id != 0) {
-            startValue += id*instanceStart;
-        }
-    } else {
-        startValue += numberToCompute%p+id*instanceStart;
-    }
+		if (id != 0) {
+			startValue += id * instanceStart;
+		}
+	}
+	else {
+		startValue += numberToCompute % p + id * instanceStart;
+	}
 
-    cout << "Thread: " << id << " starting on: " << startValue << " ending on: " << startValue+instanceStart-1 << endl;
-    for (int x = startValue; x < startValue + instanceStart; x++) {
-        cover(x, out);
-    } // end for
-    out.close();
-    
-    MPI_Finalize();
-    return 0;
+	if (id == 0) {
+		MPI_Irecv(&found_item, p, MPI_INT, MPI_ANY_SOURCE, found_tag, MPI_COMM_WORLD, &found_request);
+	}
+	else {
+		MPI_Ibcast(&found_item, 1, MPI_INT, 0, MPI_COMM_WORLD, &found_request);
+	}
+
+	if (id == 0) {
+		cout << "Thread: " << id << " starting on: " << startValue << " ending on: " << startValue + instanceStart - 1 << endl;
+		for (int x = startValue; x < startValue + instanceStart; x++) {
+			cover(x, out);
+		} // end for
+		out.close();
+	}
+	else {
+		int* differenceCover = new int[p];
+		int* testCover = new int[p];
+		int dsize = -1;
+		int cont = 1;
+
+		while (cont) {
+			MPI_Recv(&differenceCover, p, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Recv(&dsize, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+			if (coverOfSize(p, dsize, differenceCover, testCover)) {
+				MPI_Send(&differenceCover, p, MPI_INT, 0, found_tag, MPI_COMM_WORLD);
+			}
+
+			MPI_Recv(&cont, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		}
+	}
+
+	MPI_Finalize();
+	return 0;
 } // end main
 
 void cover(const int p, ofstream &out) {
-    // The min is the smallest possible difference cover of a set of size p.
-    int min = 1;
-    for (int x = 2; x < p; x++)
-        if (x * (x - 1) + 1 >= p) {
-            min = x;
-            break;
-        } // end if
+	// The min is the smallest possible difference cover of a set of size p.
+	int min = 1;
+	for (int x = 2; x < p; x++)
+		if (x * (x - 1) + 1 >= p) {
+			min = x;
+			break;
+		} // end if
 
-    // The max is the largest possible difference cover of a set of size p.
-    int max = p;
-    for (int x = 2; x < p / 2; x++)
-        if (x * x > p) {
-            max = 3 * (p + x - 1) / (2 * (x - 1)) + 4;
-            break;
-        } // end if
+	// The max is the largest possible difference cover of a set of size p.
+	int max = p;
+	for (int x = 2; x < p / 2; x++)
+		if (x * x > p) {
+			max = 3 * (p + x - 1) / (2 * (x - 1)) + 4;
+			break;
+		} // end if
 
-    int *differenceCover = new int[p];
-    int *testCover = new int[p];
-    for (int x = min; x <= max; x++)
-        if (coverOfSize(p, x, differenceCover, testCover)) {
-            print(p, differenceCover, out);
-            break;
-        } // end if
-    delete[] differenceCover;
-    delete[] testCover;
+	int *differenceCover = new int[p];
+	int *testCover = new int[p];
+
+	for (int x = min; x <= max; x++) {
+		//send the stuff for the slaves form here [look at the gray code stuf]
+		if (coverOfSize(p, x, differenceCover, testCover)) {
+			print(p, differenceCover, out);
+			break;
+		} // end if
+	}
+	delete[] differenceCover;
+	delete[] testCover;
 } // end cover
 
 int coverOfSize(const int p, int dSize, int *differenceCover, int *testCover) {
-    // differenceCover is an array that stores the current pattern generated
-    int beginning = 1;
-    while (choose(p, dSize, differenceCover, beginning))
-        if (isCover(p, differenceCover, testCover))
-            return 1;
-    return 0;
+	// differenceCover is an array that stores the current pattern generated
+	int beginning = 1;
+	int count = 0;
+	while (choose(p, dSize, differenceCover, beginning))
+		if (count % 10000 == 0) {
+			if (id == 0) {
+				int found = 0;
+				MPI_Test(&found_request, &found, MPI_STATUS_IGNORE);
+				if (found) {
+					MPI_Request bcast_request;
+					MPI_Ibcast(&found_item, 1, MPI_LONG_LONG, root, MPI_COMM_WORLD, &bcast_request);
+					MPI_Wait(&bcast_request, MPI_STATUS_IGNORE);
+					break;
+				}
+			}
+			else {
+				int found = 0;
+				MPI_Test(&found_request, &found, MPI_STATUS_IGNORE);
+				if (found) {
+					return 0;
+				}
+			}
+		}
+	if (isCover(p, differenceCover, testCover))
+		return 1;
+	return 0;
 } // end coverOfSize
 
 int choose(const int p, int dSize, int *pattern, int &beginning) {
-    static int index;
-    if (beginning) {
-        for (int x = 0; x < p; x++)
-            pattern[x] = 0;
-        for (int x = 0; x < dSize - 1; x++)
-            pattern[x] = 1;
-        pattern[p - 1] = 1;
-        index = p - 1;
-        beginning = 0;
-        return 1;
-    }
+	static int index;
+	if (beginning) {
+		for (int x = 0; x < p; x++)
+			pattern[x] = 0;
+		for (int x = 0; x < dSize - 1; x++)
+			pattern[x] = 1;
+		pattern[p - 1] = 1;
+		index = p - 1;
+		beginning = 0;
+		return 1;
+	}
 
-    if (pattern[index - 1] == 0) {
-        pattern[index] = 0;
-        pattern[--index] = 1;
-        return 1;
-    } // end if
-    else if (index != p - 1) { // && pattern[index - 1] == 1)
-        pattern[--index] = 0;
-        pattern[p - 1] = 1;
-        index = p - 1;
-        return 1;
-    } // end else if
-    else { // pattern[index - 1] == 1 && index == p - 1
-        int flipCount = 0;
-        while (pattern[--index]) {
-            pattern[index] = 0;
-            flipCount++;
-        } // end while
-        while (index > 0 && !pattern[--index]);
-        if (index == 1)
-            return 0;
-        else {
-            pattern[index] = 0;
-            flipCount++;
-            for (int y = 1; y <= flipCount; y++)
-                pattern[index + y] = 1;
-            index = p - 1;
-            return 1;
-        } // end else
-    } // end else
+	if (pattern[index - 1] == 0) {
+		pattern[index] = 0;
+		pattern[--index] = 1;
+		return 1;
+	} // end if
+	else if (index != p - 1) { // && pattern[index - 1] == 1)
+		pattern[--index] = 0;
+		pattern[p - 1] = 1;
+		index = p - 1;
+		return 1;
+	} // end else if
+	else { // pattern[index - 1] == 1 && index == p - 1
+		int flipCount = 0;
+		while (pattern[--index]) {
+			pattern[index] = 0;
+			flipCount++;
+		} // end while
+		while (index > 0 && !pattern[--index]);
+		if (index == 1)
+			return 0;
+		else {
+			pattern[index] = 0;
+			flipCount++;
+			for (int y = 1; y <= flipCount; y++)
+				pattern[index + y] = 1;
+			index = p - 1;
+			return 1;
+		} // end else
+	} // end else
 } // end choose
 
 int isCover(const int p, const int *differenceCover, int *testCover) {
-    // Takes half the time, unless the double pointer casting takes too long. (Check this.)
-    double *temp = (double *) testCover;
-    for (int x = 0; x < p / 2; x++)
-        temp[x] = 0;
-    // Faster to not check if it is even or not.
-    testCover[p - 1] = 0;
+	// Takes half the time, unless the double pointer casting takes too long. (Check this.)
+	double *temp = (double *)testCover;
+	for (int x = 0; x < p / 2; x++)
+		temp[x] = 0;
+	// Faster to not check if it is even or not.
+	testCover[p - 1] = 0;
 
-    testCover[0] = 1;
-    for (int x = 0; x < p - 1; x++)
-        if (differenceCover[x])
-            for (int y = x + 1; y < p; y++)
-                if (differenceCover[y]) {
-                    // testCover[(x - y + p) % p] = 1;
-                    // testCover[(y - x + p) % p] = 1;
-                    // Use below instead of the above, since y > x, (y-x) < p, and (y-x)%p + (x-y)%p = p
-                    int q = y - x;
-                    testCover[q] = testCover[p - q] = 1;
-                    // testCover[p - q] = 1;
-                } // end if
+	testCover[0] = 1;
+	for (int x = 0; x < p - 1; x++)
+		if (differenceCover[x])
+			for (int y = x + 1; y < p; y++)
+				if (differenceCover[y]) {
+					// testCover[(x - y + p) % p] = 1;
+					// testCover[(y - x + p) % p] = 1;
+					// Use below instead of the above, since y > x, (y-x) < p, and (y-x)%p + (x-y)%p = p
+					int q = y - x;
+					testCover[q] = testCover[p - q] = 1;
+					// testCover[p - q] = 1;
+				} // end if
 
-    if (size(testCover, p) == p)
-        return 1;
-    return 0;
+	if (size(testCover, p) == p)
+		return 1;
+	return 0;
 } // end isCover
 
 void print(const int p, const int *differenceCover, ofstream &out) {
-    cout << setw(4) << p;
-    out << setw(4) << p;
-    int f = size(differenceCover, p);
-    cout << setw(9) << f;
-    out << setw(9) << f;
-    cout << setw(8);
-    out << setw(8);
-    for (int x = 0; x < p; x++)
-        if (differenceCover[x]) {
-            cout << x << setw(3);
-            out << x << setw(3);
-        } // end if
-    cout << endl;
-    out << endl;
+	cout << setw(4) << p;
+	out << setw(4) << p;
+	int f = size(differenceCover, p);
+	cout << setw(9) << f;
+	out << setw(9) << f;
+	cout << setw(8);
+	out << setw(8);
+	for (int x = 0; x < p; x++)
+		if (differenceCover[x]) {
+			cout << x << setw(3);
+			out << x << setw(3);
+		} // end if
+	cout << endl;
+	out << endl;
 } // end print
 
 int size(const int *cover, const int p) {
-    int f = 0;
-    for (int x = 0; x < p; x++)
-        f += cover[x];
-    return f;
+	int f = 0;
+	for (int x = 0; x < p; x++)
+		f += cover[x];
+	return f;
 } // end size
