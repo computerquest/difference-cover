67a187a1d67d610ad860b605cf8a49a21fc5cf65
J Stigter <jaredstigter@gmail.com>
2018-12-11 20:04:50 -0500

    got burst mode going

---
+++ ConsoleApplication1/ConsoleApplication1/test.sh
@@ -0,0 +1,14 @@
+while true
+do
+	a=$(nproc --all)
+	b=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)/($2+$4+$5)*100} END {print usage}')
+	b=${b%.*}
+	c=$((100-$b))
+	d=$(($a*$c))
+	e=$(($d/100))
+	if (( $e == 0 )); then
+		e=1
+	fi
+	echo $e
+        mpirun -np $e blah solutions.txt 20 1000 1000000
+done
