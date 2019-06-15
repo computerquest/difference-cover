b08fa33e160f4aa94d7da83c54725a9bc90fe286
Jared Stigter <spamacct517@@gmail.com>
2019-06-15 12:06:50 -0400

    mostly done

--- main.h
+++ main.h
@@ -1,27 +1,44 @@
 //
 // Created by jstigter on 6/10/19.
 //
-
+#include <vector>
+using namespace std;
 #ifndef DIFFERENCE_COVER_MAIN_H
 #define DIFFERENCE_COVER_MAIN_H
 
 
-void updateTest();
+void updateTest(int num);
 
-void isCover();
+int isCover();
 
-void undoTest();
+void undoTest(int num);
+
 void quickCheck();
 
-void nextCover();
+int nextCover();
+
+int push(int num);
+
+int pop(int num);
+
+int testSize();
 
 int searchCovers(int localThird, int localdSize, bool perfect);
+
 void startSearch();
+
+void popLayer();
 
 void calcBounds(unsigned long long numNum, unsigned long long &iters, unsigned long long &starting);
 
 unsigned long long gcd(unsigned long long x, unsigned long long y);
 
 unsigned long long nChoosek(unsigned long long n, unsigned long long k);
+
+int generateCover(int localdSize, int minSize, int stop);
+
+vector<int> kthCombination(unsigned long long k, vector<int> l, int r);
+
+bool check();
 
 #endif //DIFFERENCE_COVER_MAIN_H
