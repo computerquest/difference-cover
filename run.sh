c7424fed9c8b653dc13c8d0503974937e4843a85
computerquest <jaredstigter@gmail.com>
2019-07-07 08:24:31 -0700

    reduced some bloat fixed the memory error by expanding vector to accomodate out of bounds request that was made using []

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ clear
 rm [1-9]* stuff.txt
 mpicxx \-o main main.cpp && echo "Compilation Completed"
 #mpiexec -np 4 ./main stuff.txt 13 77      | 78
-mpiexec -np 1 ./main stuff.txt 93 1
+mpiexec -np 1 ./main stuff.txt 34 1
