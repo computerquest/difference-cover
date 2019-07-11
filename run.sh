8a89677c8dbf946d7d0d6225a3d04cc7b08249de
computerquest <jaredstigter@gmail.com>
2019-07-10 19:05:02 -0700

    all push

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ clear
 rm [1-9]* stuff.txt
 mpicxx \-o main main.cpp && echo "Compilation Completed"
 #mpiexec -np 4 ./main stuff.txt 13 77      | 78
-mpiexec -np 25 ./main stuff.txt 27 1
+mpiexec -np 4 ./main stuff.txt 13 100
