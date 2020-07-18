562a315f0cda82d4eba84ccdcae745e28b85f365
Jared Stigter <jaredstigter@gmail.com>
2020-07-18 11:08:43 -0700

    updated ignore removed dumb files added counting

---
+++ main.cpp
@@ -0,0 +1,228 @@
+// Ankur Gupta
+// Mathematics Honors Thesis
+// Difference Covers
+// 8 May 2000
+
+/*
+Jared Stigter 
+Fun
+Difference Covers
+16 September 2018
+*/
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+void cover(const int p, ofstream &out);
+int coverOfSize(const int p, int dSize, int *differenceCover, int *testCover);
+int choose(const int p, int dSize, int *pattern, int &beginning);
+int isCover(const int p, const int *differenceCover, int *testCover);
+void print(const int p, const int *differenceCover, ofstream &out);
+inline int size(const int *cover, const int p);
+unsigned long long count = 0;
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
+        cout << "This program computes difference covers of a range specified by" << endl;
+        cout << "the user. This program then saves the covers to a file that you" << endl;
+        cout << "specify. The following format is used to execute this command." << endl;
+    }
+    if (argc < 3)
+    {
+        cout << "Usage: mhonors <filename> <startValue> [numberToCompute]" << endl
+             << endl;
+        cout << "<filename> - enter a filename." << endl
+             << endl;
+        cout << "<startValue> - enter an integer number. This is the first cover" << endl;
+        cout << "               the program will attempt to find." << endl
+             << endl;
+        cout << "[numberToCompute] - enter an integer number. This field is optional, and" << endl;
+        cout << "                    will tell the program to compute this many covers," << endl;
+        cout << "                    starting at <startValue>. If you choose not to provide" << endl;
+        cout << "                    this value, the default value is 10." << endl;
+        return 0;
+    }
+
+    int startValue = atoi(argv[2]);
+    int numberToCompute = 10;
+    if (argc == 4)
+        numberToCompute = atoi(argv[3]);
+    ofstream out;
+    string outFile = argv[1];
+    out.open(outFile, ios::out);
+    if (!out)
+    {
+        cerr << "File could not be opened." << endl;
+        exit(1);
+    } // end if
+    cout << setw(4) << "p" << setw(9) << "f(p)" << setw(37) << "difference cover" << endl;
+    out << setw(4) << "p" << setw(9) << "f(p)" << setw(37) << "difference cover" << endl;
+
+    for (int x = startValue; x < startValue + numberToCompute; x++)
+    {
+        count = 0;
+        cover(x, out);
+        ofstream count_out;
+        count_out.open((outFile + "_count").c_str(), std::fstream::app);
+        cout << " total: " << count << endl;
+        count_out << x << "," << count << endl;
+    } // end for
+    out.close();
+    return 0;
+} // end main
+
+void cover(const int p, ofstream &out)
+{
+    // The min is the smallest possible difference cover of a set of size p.
+    int min = 1;
+    for (int x = 2; x < p; x++)
+        if (x * (x - 1) + 1 >= p)
+        {
+            min = x;
+            break;
+        } // end if
+
+    // The max is the largest possible difference cover of a set of size p.
+    int max = p;
+    for (int x = 2; x < p / 2; x++)
+        if (x * x > p)
+        {
+            max = 3 * (p + x - 1) / (2 * (x - 1)) + 4;
+            break;
+        } // end if
+
+    int *differenceCover = new int[p];
+    int *testCover = new int[p];
+    for (int x = min; x <= max; x++)
+        if (coverOfSize(p, x, differenceCover, testCover))
+        {
+            print(p, differenceCover, out);
+            break;
+        } // end if
+    delete[] differenceCover;
+    delete[] testCover;
+} // end cover
+
+int coverOfSize(const int p, int dSize, int *differenceCover, int *testCover)
+{
+    // differenceCover is an array that stores the current pattern generated
+    int beginning = 1;
+    while (choose(p, dSize, differenceCover, beginning))
+        if (isCover(p, differenceCover, testCover))
+            return 1;
+    return 0;
+} // end coverOfSize
+
+int choose(const int p, int dSize, int *pattern, int &beginning)
+{
+    static int index;
+    if (beginning)
+    {
+        for (int x = 0; x < p; x++)
+            pattern[x] = 0;
+        for (int x = 0; x < dSize - 1; x++)
+            pattern[x] = 1;
+        pattern[p - 1] = 1;
+        index = p - 1;
+        beginning = 0;
+        return 1;
+    }
+
+    if (pattern[index - 1] == 0)
+    {
+        pattern[index] = 0;
+        pattern[--index] = 1;
+        return 1;
+    } // end if
+    else if (index != p - 1)
+    { // && pattern[index - 1] == 1)
+        pattern[--index] = 0;
+        pattern[p - 1] = 1;
+        index = p - 1;
+        return 1;
+    } // end else if
+    else
+    { // pattern[index - 1] == 1 && index == p - 1
+        int flipCount = 0;
+        while (pattern[--index])
+        {
+            pattern[index] = 0;
+            flipCount++;
+        } // end while
+        while (index > 0 && !pattern[--index])
+            ;
+        if (index == 0)
+            return 0;
+        else
+        {
+            pattern[index] = 0;
+            flipCount++;
+            for (int y = 1; y <= flipCount; y++)
+                pattern[index + y] = 1;
+            index = p - 1;
+            return 1;
+        } // end else
+    }     // end else
+} // end choose
+
+int isCover(const int p, const int *differenceCover, int *testCover)
+{                                       //difference cover and test cover are binary, p is n,
+                                        // Takes half the time, unless the double pointer casting takes too long. (Check this.)
+    double *temp = (double *)testCover; //what is the purpose of having this
+    for (int x = 0; x < p / 2; x++)
+        temp[x] = 0;
+    // Faster to not check if it is even or not.
+    testCover[p - 1] = 0;
+
+    //this does all the mod operations to find which numbers are generated
+    testCover[0] = 1;
+    for (int x = 0; x < p - 1; x++)
+        if (differenceCover[x])
+            for (int y = x + 1; y < p; y++)
+                if (differenceCover[y])
+                {
+                    // testCover[(x - y + p) % p] = 1;
+                    // testCover[(y - x + p) % p] = 1;
+                    // Use below instead of the above, since y > x, (y-x) < p, and (y-x)%p + (x-y)%p = p x=0 y=2 n=100 2+98 so works
+                    int q = y - x;
+                    testCover[q] = testCover[p - q] = 1;
+                    // testCover[p - q] = 1;
+                } // end if
+
+    count++;
+
+    if (size(testCover, p) == p) //could optimize so it can exit early if there is a zero
+        return 1;
+    return 0;
+} // end isCover
+
+void print(const int p, const int *differenceCover, ofstream &out)
+{
+    cout << setw(4) << p;
+    out << setw(4) << p;
+    int f = size(differenceCover, p);
+    cout << setw(9) << f;
+    out << setw(9) << f;
+    cout << setw(8);
+    out << setw(8);
+    for (int x = 0; x < p; x++)
+        if (differenceCover[x])
+        {
+            cout << x << setw(3);
+            out << x << setw(3);
+        } // end if
+    cout << endl;
+    out << endl;
+} // end print
+
+int size(const int *cover, const int p)
+{ //counts number of 1s (number of elements)
+    int f = 0;
+    for (int x = 0; x < p; x++)
+        f += cover[x];
+    return f;
+} // end size
